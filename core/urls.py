379f8cefac2a71681c19caa9315721d583b5336a
alvaro <alvaro.villalobos@uc.cl>
2024-01-24 16:13:52 -0300

    v12

--- core/urls.py
+++ core/urls.py
@@ -33,6 +33,7 @@ schema_view = get_schema_view(
 
 urlpatterns = [
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
 ]
