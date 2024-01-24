a6f734a9cdf96845b990fd3a7226dd70734311ba
alvaro <alvaro.villalobos@uc.cl>
2024-01-24 15:27:21 -0300

    v5

--- core/settings.py
+++ core/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-
+    'drf_yasg',
     'rest_framework',
     'app',
 ]
--- core/urls.py
+++ core/urls.py
@@ -14,10 +14,28 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
 
+# Configuración de la vista de esquema de Swagger
+schema_view = get_schema_view(
+   openapi.Info(
+      title="API de Empresas",
+      default_version='v1',
+      description="Documentación de la API de Empresas",
+      terms_of_service="https://www.tuempresa.com/policies/terms/",
+      contact=openapi.Contact(email="contacto@tuempresa.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
+    path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('admin/', admin.site.urls),
     path('api/', include('app.urls')),
 ]
